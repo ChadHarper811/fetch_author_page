c1d289f8b9a385ca5fbb97ba52629d400eed0144
Chad Harper <chadharper811@gmail.com>
2024-10-20 13:25:16 -0500

    added divider to cards and made bio only 50 characters with longer bio having ellipsis ending

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ const fetchMoreAuthors = () => {
     startingIndex += 8;
     endingIndex += 8;
     displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
-    if (authorDataArr.length <= endingIndex) {}
+    if (authorDataArr.length <= endingIndex) {
+        loadMoreBtn.disabled = true;
+        loadMoreBtn.textContent = "No more data to load";
+    };
 };
 
 // const loadMoreAuthors = () => {
@@ -59,7 +62,8 @@ const displayAuthors = (authors) => {
         <div id="${index}" class="user-card ${index >= endingIndex ? "hidden" : ""}">
             <h2 class="author-name">${author}</h2>
             <img class="user-img" src="${image}" alt="${author} avatar" />
-            <p class="bio">${bio}</p>
+            <div class="purple-divider"></div>
+            <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + "..." : bio}</p>
             <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
         </div>
         `;
