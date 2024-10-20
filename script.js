fbcee884756787c0a2b770febb314c0e5c14137a
Chad Harper <chadharper811@gmail.com>
2024-10-20 13:12:14 -0500

    made page functions for displaying more and less authors with new load less button and event listeners

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const authorContainer = document.getElementById("author-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
+const loadLessBtn = document.getElementById("load-less-btn");
 
 let startingIndex = 0;
 let endingIndex = 8;
@@ -9,20 +10,53 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     .then((res) => res.json())
     .then((data) => {
         authorDataArr = data;
-        displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+        // displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+        displayAuthors(authorDataArr);
     })
     .catch((err) => {console.error(`There was an error: ${err}`)});
 
-const fetchMoreAuthors = () => {
-    startingIndex += 8;
-    endingIndex += 8;
-    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+// const fetchMoreAuthors = () => {
+//     startingIndex += 8;
+//     endingIndex += 8;
+//     displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+// };
+
+const loadMoreAuthors = () => {
+    if (endingIndex + 8 >= authorDataArr.length) {
+        endingIndex = authorDataArr.length
+        loadLessBtn.classList.remove("hidden");
+        loadMoreBtn.classList.add("hidden");
+    } else if (8 <= endingIndex <= authorDataArr.length) {
+        endingIndex += 8;
+        loadLessBtn.classList.remove("hidden");
+    }
+
+    for (let i = 0; i < endingIndex; i++) {
+        let cardToDisplay = document.getElementById(i);
+        cardToDisplay.classList.remove("hidden");
+    }
+};
+
+const loadLessAuthors = () => {
+    if (endingIndex - 8 <= 8) {
+        endingIndex = 8;
+        loadLessBtn.classList.add("hidden");
+        loadMoreBtn.classList.remove("hidden");
+    } else if (8 <= endingIndex <= authorDataArr.length) {
+        endingIndex -= 8;
+    }
+
+    for (let i = endingIndex; i < authorDataArr.length; i++) {
+        let cardToHide = document.getElementById(i);
+        cardToHide.classList.add("hidden");
+    }
+
 };
 
 const displayAuthors = (authors) => {
     authors.forEach(({author, image, url, bio}, index) => {
         authorContainer.innerHTML += `
-        <div id="${index}" class="user-card">
+        <div id="${index}" class="user-card ${index >= endingIndex ? "hidden" : ""}">
             <h2 class="author-name">${author}</h2>
             <img class="user-img" src="${image}" alt="${author} avatar" />
             <p class="bio">${bio}</p>
@@ -31,3 +65,8 @@ const displayAuthors = (authors) => {
         `;
     });
 };
+
+// loadMoreBtn.addEventListener("click", fetchMoreAuthors);
+
+loadMoreBtn.addEventListener("click", loadMoreAuthors);
+loadLessBtn.addEventListener("click", loadLessAuthors);
